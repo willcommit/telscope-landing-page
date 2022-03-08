b42d7025995fae93d54b82e355a145cccbf10ee6
William Elfversson <welfversson@gmail.com>
2022-03-08 15:45:25 +0100

    possible fix

--- src/lib/modal.svelte
+++ src/lib/modal.svelte
@@ -42,8 +42,6 @@
         display: flex;
         justify-content: right;
         overflow-x: hidden;
-        grid-template-columns: auto auto auto auto auto;
-        grid-template-rows: auto auto auto auto;
         position: absolute;
         z-index: 40;
         left: 0;
